0a4f152079580017ddfb6f6d24e91040c0256abb
adriancarretero19 <carreteromateoadrian@gmail.com>
2026-01-22 18:52:01 +0100

    Primera versión

---
+++ portafolio.js
@@ -0,0 +1,9 @@
+let btnCurriculum = document.getElementById("curriculum");
+let btnProyectos = document.getElementById("proyectos");
+
+btnCurriculum.addEventListener("click", function(){
+    window.open("curriculum.png", "_blank");
+});
+btnProyectos.addEventListener("click", function(){
+    window.open("https://github.com/adriancarretero19", "_blank");
+});
